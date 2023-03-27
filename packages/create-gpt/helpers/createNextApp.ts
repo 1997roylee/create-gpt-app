e6b32c7d60976416de7d42e6ca39643ba3d42b41
Roy <53046899+1997roylee@users.noreply.github.com>
2023-03-28 02:26:30 +0800

    rename

--- packages/create-gpt/helpers/createNextApp.ts
+++ packages/create-gpt/helpers/createNextApp.ts
@@ -1,7 +1,7 @@
 import retry from 'async-retry'
 import { downloadExample } from './example'
 
-export const createApp = async (rootPath: string) => {
+export const createNextApp = async (rootPath: string) => {
     await retry(() => downloadExample(rootPath, "examples/next-typescript"), {
         retries: 3,
     })
